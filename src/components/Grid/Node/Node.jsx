44b6f7127ca4a3a77a008dd09758b4bc9b6d9f5d
Stephen Palazzo <stephencpalazzo@gmail.com>
2022-07-11 00:02:39 -0400

    Baisc Pathfinder working

--- src/components/Grid/Node/Node.jsx
+++ src/components/Grid/Node/Node.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Node.css';
 
-export default function Node({ row, column, isStart, isEnd }) {
-  const nodeType = isStart ? 'node-start' : isEnd ? 'node-end' : '';
+export default function Node({
+  row,
+  column,
+  isStart,
+  isEnd,
+  isWall,
+  isVisited,
+}) {
+  const nodeType = isStart
+    ? 'node-start'
+    : isEnd
+    ? 'node-end'
+    : isWall
+    ? 'node-wall'
+    : isVisited
+    ? 'node-visited'
+    : '';
 
   return <div id={`${row}-${column}`} className={`node ${nodeType}`}></div>;
 }
